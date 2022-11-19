191f177be5f56b47112a4fadf3ed12734ef316df
YusungKim <yusungkim@me.com>
2022-11-19 11:04:29 +0900

    tetris with one figure

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Tetris
+
+
+## Source
+
+[参考 Youtube](https://www.youtube.com/watch?v=7kGNs5R-AM8)
+
+
+Figure coordinate
+![figure](images/figure_coordinate.png)
+
+
+## 開発環境
+
+```bash
+python --version
+# Python 3.9.15
+
+source .venv/bin/activate
+
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,75 @@
+import pygame
+import copy
+
+W, H = 10, 20
+TILE = 45
+GAME_RES = W * TILE, H * TILE
+FPS = 60
+
+pygame.init()
+screen = pygame.display.set_mode(GAME_RES)
+clock = pygame.time.Clock()
+
+grid = [
+  pygame.Rect(x * TILE, y * TILE, TILE, TILE)
+  for x in range(W)
+  for y in range(H)
+]
+
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+# Create figure at the position of (center x, 2nd line from the top)
+figures = [
+  [pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos]
+  for fig_pos in figures_pos
+]
+figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
+figure = copy.deepcopy(figures[0])
+
+def out_of_borders(figure):
+  return any([tile.x < 0 or tile.x > W -1 for tile in figure])
+
+while True:
+  dx = 0
+  screen.fill(pygame.Color('black'))
+
+  # Control
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      exit()
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        dx = -1
+      elif event.key == pygame.K_RIGHT:
+        dx = 1
+
+    # move x
+    figure_old = copy.deepcopy(figure)
+    for i in range(4):
+      figure[i].x += dx
+      if out_of_borders(figure):
+        figure = copy.deepcopy(figure_old)
+        break
+
+    # draw grid
+    [
+      pygame.draw.rect(screen, (40, 40, 40), i_rect, 1)
+      for i_rect in grid
+    ]
+
+    # draw figure
+    for i in range(4):
+      figure_rect.x = figure[i].x * TILE
+      figure_rect.y = figure[i].y * TILE
+      pygame.draw.rect(screen, pygame.Color('white'), figure_rect)
+  
+    pygame.display.update() # display Surface全体を更新して画面に描写します
+    pygame.display.set_caption("Tetris, FPS=" + str(round(clock.get_fps(), 3)))
+    clock.tick(FPS)
