70a418780644b65c5851efa2f05a97aeec666d9a
YusungKim <yusungkim@me.com>
2022-11-20 12:18:04 +0900

    score system

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from figure import *
-from random import choice
+from random import choice, randint
 from copy import deepcopy
+from termcolor import colored, cprint
 
 # Screen Configs
 W, H = 10, 20 # count of tiles
@@ -11,7 +12,7 @@ FPS = 60      # frame per sec
 
 # Game Configs
 FALLING_SPEED = 20
-FALLING_SPEED_ACCELERATED = 200 # when key down
+FALLING_SPEED_ACCELERATED = 400 # when key down
 FALLING_TRIGGER = 1000          # falling_count reaches this, fall one unit
 
 # Grid borders
@@ -25,27 +26,41 @@ GRID = [
 figures = Figure.all_shapes(initial_pos=(W // 2, 2))
 field = [[False for i in range(H)] for j in range(W)]
 
-def random_figure():
-  figure = deepcopy(choice(figures))
-  figure.print()
-  return figure
-
 def figure_rect(x = 0, y = 0):
   return pygame.Rect(x * TILE, y * TILE, TILE - 2, TILE - 2)
 
 ################################### Game start
 
 pygame.init()
+pygame.display.set_caption("Tetris, YusungKim")
+pygame.display.set_icon(pygame.image.load('images/meteor.png'))
 screen = pygame.display.set_mode(GAME_RES)
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()  
+
+scores = {
+  "completed": 0,   # clear one or more line for each figure
+  "combo": 0,   # continously completed
+  "score": 0,   # total score
+}
+
+def new_figure():
+  # reset
+  if scores["completed"] == 0:
+    scores["combo"] = 0
+  scores["completed"] = 0
+
+  # create new figure
+  figure = deepcopy(choice(figures))
+  figure.print()
+  return figure
 
 falling_count, fast_falling = 0, False
-figure = random_figure()
+figure = new_figure()
 while True:
   dx, dy = 0, 1
   screen.fill(pygame.Color('black'))
 
-  # Control
+  ################ Control
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       exit()
@@ -60,8 +75,12 @@ while True:
       elif event.key == pygame.K_DOWN:
         fast_falling = True
       elif event.key == pygame.K_UP:
-        print("U", end=" ")
-        figure.rotate(field)
+        if fast_falling:
+          fast_falling = False
+        else:
+          print("U", end=" ")
+          # rotate 90 degrees
+          figure.rotate(field)
 
     # move x
     figure.move(Direction.X, dx, field)
@@ -79,8 +98,62 @@ while True:
     for tile in figure.tiles:
       field[tile.x][tile.y] = figure.color
     # create new figure
-    figure = random_figure()
+    figure = new_figure()
     fast_falling = False
+
+  # check completed lines
+  # if completed, rewrite that line with above line
+  completed = 0
+  rewrite_y = H - 1
+  for source_y in range(H - 1, -1, -1):
+    filled = 0
+    for x in range(W):
+      if field[x][source_y]:
+        filled += 1
+      field[x][rewrite_y] = field[x][source_y]
+    if filled == W:
+      completed += 1
+    else:
+      rewrite_y -= 1
+
+  # score
+  if completed > 0:
+    combo_msg, completed_msg, multi_msg = '', '', ''
+    combo_bonus, completed_bonus, multi_bonus = 0, 0, 0
+
+    # combo bonus
+    combo = scores["combo"] + 1
+    if combo > 1:
+      combo_bonus = combo * 10
+      combo_msg = f" +{combo_bonus} Pts ({combo} Combo!)"
+
+    # complete bonus
+    completed_bonus = completed * 2
+    completed_msg = f" +{completed_bonus} Pts"
+
+    # multiple complete bonus
+    if completed > 1:
+      multi_bonus = randint(1, completed) * randint(completed, 5) * completed
+      multi_msg = f" +{multi_bonus} Pts ({completed} Multi!)"
+      
+    score = scores["score"] + completed_bonus + multi_bonus + combo_bonus
+
+    print(
+      colored(f"SCORE: {str(score).ljust(5, ' ')}", 'red') \
+      + colored(completed_msg, 'yellow') \
+      + colored(multi_msg, 'magenta') \
+      + colored(combo_msg, 'green')
+    )
+
+    # update
+    scores["combo"] = combo
+    scores["completed"] = completed
+    scores["score"] = score
+
+    # update title
+    pygame.display.set_caption(f"Tetris, YusungKim   {score} Points")
+
+  ############# Draw
 
   # draw grid
   [
@@ -106,5 +179,4 @@ while True:
         pygame.draw.rect(screen, filled, figure_rect(x, y))
 
   pygame.display.update() # display Surface全体を更新して画面に描写します
-  pygame.display.set_caption("Tetris, FPS=" + str(round(clock.get_fps(), 3)))
   clock.tick(FPS)
